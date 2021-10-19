2a13f0e4a9e5d322bf66fe77444ac4c0f71b89ae
ezza022 <alfirman.pahlepi@gmail.com>
2021-10-19 21:29:17 +0700

    update: styling home

--- components/Layout.js
+++ components/Layout.js
@@ -2,7 +2,7 @@ import Header from "./Header";
 
 export default function Layout({ children }) {
   return (
-    <div className="text-gray-700 roboto bg-blue-100">
+    <div className="text-gray-700 roboto">
       <Header />
       <main className="w-11/12 mx-auto">{children}</main>
     </div>
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,91 @@
+import { useState } from "react";
 import Layout from "../components/Layout";
-import NewsCard from "../components/NewsCard";
+// import NewsCard from "../components/NewsCard";
 
 export default function Home() {
+  const [activeTab, setActiveTab] = useState("latest");
   return (
     <Layout>
-      <div>
-        <h1 className="text-4xl font-bold text-center">news</h1>
-        <div className="grid grid-cols-4 gap-6">
-          {[1, 1, 1, 1, 1, 1, 1, 1].map((e, i) => (
-            <NewsCard key={i} />
-          ))}
+      <div className=" flex space-x-6 h-[200vh]">
+        <div className="flex-[3]">
+          <div className="flex space-x-6">
+            <div className="flex-1 relative">
+              <h5 className="flex justify-center absolute top-0 w-full">
+                <span className="oswald uppercase text-lg px-3 bg-[#FF005B] text-white skew-x-[-15deg] font-extrabold">
+                  Programming
+                </span>
+              </h5>
+              <div className="border-t border-black mt-3 pt-7 space-y-7">
+                {[1, 2].map((el) => (
+                  <div key={el}>
+                    <div className="bg-red-500 h-[155px]"></div>
+                    <p className="font-extrabold uppercase text-sm mt-2">
+                      Programming{" "}
+                      <span className="font-medium lowercase">
+                        / 2 years ago
+                      </span>
+                    </p>
+                    <h4 className="oswald font-extrabold text-2xl text-gray-800">
+                      Emily Ratajkowski channels back-to-school style
+                    </h4>
+                  </div>
+                ))}
+              </div>
+            </div>
+            <div className="flex-[2] bg-purple-700 h-[600px]">hero img</div>
+          </div>
+          <div className="ring">asd</div>
+        </div>
+        <div className="flex-1 mt-10 ring">
+          <div className="sticky top-16">
+            <div className="flex justify-center space-x-px">
+              {tabs.map((tab, i) => (
+                <button
+                  key={i}
+                  onClick={() => setActiveTab(tab.text)}
+                  className={`${
+                    tab.text === activeTab
+                      ? "bg-[#FF005B] text-white"
+                      : "bg-transparent text-gray-400 hover:text-gray-800"
+                  } oswald rounded-none uppercase text-lg px-3 skew-x-[-15deg] font-extrabold`}
+                >
+                  {tab.text}
+                </button>
+              ))}
+            </div>
+            <div className="w-full space-y-4 mt-4">
+              {[1, 2, 3].map((el) => (
+                <div key={el} className="flex space-x-3">
+                  <div className="h-24 w-24 flex-grow-0 bg-green-600"></div>
+                  <div className="w-[calc(100%-6rem)]">
+                    <p className="font-extrabold uppercase text-sm mt-2">
+                      Programming{" "}
+                      <span className="font-medium lowercase">
+                        / 2 years ago
+                      </span>
+                    </p>
+                    <h5 className="oswald font-extrabold text-gray-800">
+                      Emily Ratajkowski channels badsadasdasdasasdsada
+                    </h5>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
         </div>
       </div>
     </Layout>
   );
 }
+
+const tabs = [
+  {
+    text: "latest",
+  },
+  {
+    text: "trending",
+  },
+  {
+    text: "popular",
+  },
+];
