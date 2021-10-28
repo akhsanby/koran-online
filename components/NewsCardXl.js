a5ae0d45cd20ab6ad00ae058d23d7ed779f0605c
ezza022 <alfirman.pahlepi@gmail.com>
2021-10-28 17:42:55 +0700

    fix: responsive NewsCardXl

--- components/NewsCardXl.js
+++ components/NewsCardXl.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function NewsCardXl() {
   return (
     <a href="#" className="flex group">
-      <div className="h-[120px] xs:h-[180px] lg:h-[229px] w-2/5 md:w-1/2 relative bg-black">
+      <div className="h-[120px] xs:h-[180px] lg:h-[229px] w-2/5 md:w-1/2 lg:w-1/3 relative bg-black">
         <Image
           alt="random-pic"
           layout="fill"
@@ -12,7 +12,7 @@ export default function NewsCardXl() {
           className="group-hover:opacity-80 duration-300"
         />
       </div>
-      <div className="h-[120px] xs:h-[180px] lg:h-[229px] w-3/5 md:w-1/2 px-4 lg:px-6 text-justify xs:border-t border-gray-300 flex flex-col lg:justify-center">
+      <div className="h-[120px] xs:h-[180px] lg:h-[229px] w-3/5 md:w-1/2 lg:w-2/3 px-4 lg:px-6 text-justify xs:border-t border-gray-300 flex flex-col lg:justify-center">
         <p className="font-extrabold uppercase text-xs lg:text-sm xs:mt-4 lg:mt-2 mb-1 lg:mb-0">
           Programming{" "}
           <span className="font-medium lowercase">/ 2 years ago</span>
