462545dd9f28905c56c2d28bf9eddec08e43b1ad
AkhsanBy <akhsanby@gmail.com>
2021-11-22 19:07:36 +0700

    add function hook to set title on page

--- src/utils/index.js
+++ src/utils/index.js
@@ -31,3 +31,20 @@ export const handleRedirectToDetailNews = (dispatch, router, article) => {
 };
 
 export const handleBookmarkAndUnBookmark = (dispatch, article) => dispatch(saveThisNews({ article }));
+
+export const setNewsTitlePage = (router) => {
+  if (router.pathname === '/') return 'News | Indonesia'
+  if (router.pathname === '/indonesia/[detail]') return 'Detail News | Indonesia'
+  if (router.pathname === '/programming') return 'News | Programming'
+  if (router.pathname === '/programming/[detail]') return 'Detail News | Programming'
+  if (router.pathname === '/covid19') return 'News | Covid 19'
+  if (router.pathname === '/covid19/[detail]') return 'Detail News | Covid 19'
+  if (router.pathname === '/entertainment') return 'News | Entertainment'
+  if (router.pathname === '/entertainment/[detail]') return 'Detail News | Entertainment'
+  if (router.pathname === '/sports') return 'News | Sports'
+  if (router.pathname === '/sports/[detail]') return 'Detail News | Sports'
+  if (router.pathname === '/technology') return 'News | Technology'
+  if (router.pathname === '/technology/[detail]') return 'Detail News | Technology'
+  if (router.pathname === '/search' && router.query.keyword) return 'Search Page'
+  if (router.pathname === '/saved') return 'Saved News Page'
+}
