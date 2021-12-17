7b11b93e93bd559e2b43a284fb59a56d1fc3d822
AkhsanBy <akhsanby@gmail.com>
2021-12-17 15:47:48 +0700

    fix name is undefined when build time

--- src/components/DetailNews.jsx
+++ src/components/DetailNews.jsx
@@ -18,7 +18,7 @@ export default function DetailNews() {
     <>
       <h5 className="my-5">
         <span className="oswald uppercase text-lg px-3 bg-[#FF005B] text-white skew-x-[-15deg] font-extrabold inline-block">
-          {detailNews?.source.name}
+          {detailNews?.source?.name}
         </span>
       </h5>
       <h1 className="oswald font-bold text-3xl xs:text-5xl text-gray-900">
