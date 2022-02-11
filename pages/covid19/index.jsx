cb9b6a70b98d5ae8b7e9a9e3657253fa17a4a4d0
AkhsanBy <akhsanby@gmail.com>
2022-02-11 22:06:57 +0700

    fetch data api with getStaticProps

--- pages/covid19/index.jsx
+++ pages/covid19/index.jsx
@@ -1,10 +1,32 @@
+// componenets
 import Layout from "../../src/components/Layout";
 import MainContent from "../../src/components/MainContent";
 
-export default function Covid19() {
+// api
+import { fetchNewsCovid19 } from "../../src/utils";
+
+// redux
+import { useDispatch } from "react-redux";
+import { addNewsCovid19 } from "../../src/features/news-slice";
+
+export default function Covid19({ newsCovid19 }) {
+  const dispatch = useDispatch();
+  dispatch(addNewsCovid19(newsCovid19));
+
   return (
     <Layout title="News | Covid 19">
-      <MainContent categoryName="Covid 19" />
+      <MainContent category="Covid 19" />
     </Layout>
   );
+}
+
+export async function getStaticProps() {
+  const data = await fetchNewsCovid19();
+
+  return {
+    props: {
+      newsCovid19: data,
+    },
+    revalidate: 86400, // 1 day
+  };
 }
--- pages/technology/index.jsx
+++ pages/technology/index.jsx
@@ -1,10 +1,32 @@
+// componenets
 import Layout from "../../src/components/Layout";
 import MainContent from "../../src/components/MainContent";
 
-export default function Technology() {
+// api
+import { fetchNewsTechnology } from "../../src/utils";
+
+// redux
+import { useDispatch } from "react-redux";
+import { addNewsTechnology } from "../../src/features/news-slice";
+
+export default function Technology({ newsTechnology }) {
+  const dispatch = useDispatch();
+  dispatch(addNewsTechnology(newsTechnology));
+
   return (
     <Layout title="News | Technology">
-      <MainContent categoryName="Technology" />
+      <MainContent category="Technology" />
     </Layout>
   );
+}
+
+export async function getStaticProps() {
+  const data = await fetchNewsTechnology();
+
+  return {
+    props: {
+      newsTechnology: data,
+    },
+    revalidate: 86400, // 1 day
+  };
 }
